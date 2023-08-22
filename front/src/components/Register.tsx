0d4354aa5f747c587c74e3e2ca1100958e641a57
failaip12 <86018517+failaip12@users.noreply.github.com>
2023-08-22 13:34:44 +0200

    Implement registering

--- front/src/components/Register.tsx
+++ front/src/components/Register.tsx
@@ -1,4 +1,6 @@
 import './register.css'
+import { useState } from 'react';
+
 
 type RegisterProps = {
     isShowRegister : boolean;
@@ -6,20 +8,45 @@ type RegisterProps = {
 }
 
 const Register = ({ isShowRegister, onCloseRegister} : RegisterProps) => {
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+  //const [isRegisterSuccessful, setIsRegisterSuccessful] = useState(false);
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({ email: email, password: pass })
+    };
+    fetch('http://localhost:8000/auth/register', requestOptions)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Registration failed'); // Throw an error to handle the unsuccessful response
+        }
+        return response;
+      })
+      .then(data => {
+          //setIsRegisterSuccessful(true);
+          console.log(data);
+      })
+      .catch(error => {
+        console.log(error); // Handle the error appropriately (e.g., show an error message)
+      });
+  }
   return (
     <div className={`${isShowRegister ? "active" : ""} show`}>
       <div className="register-form">
         <div className="form-box solid">
         <button className="close-button" onClick={onCloseRegister}>X</button> {/* Close button */}
-          <form>
+          <form onSubmit={handleSubmit}>
             <h1 className="register-text">Register</h1>
-            <label>Username</label>
+            <label>E-mail</label>
             <br></br>
-            <input type="text" name="username" className="register-box" />
+            <input className="register-box" value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
             <br></br>
             <label>Password</label>
             <br></br>
-            <input type="password" name="password" className="register-box" />
+            <input className="register-box"  value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
             <br></br>
             <input type="submit" value="REGISTER" className="register-btn" />
           </form>
